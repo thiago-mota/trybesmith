1acb884b83ecb717594302665cea44848557bbbc
Thiago Mota <th.araujomota@gmail.com>
2022-09-27 19:44:10 -0300

    refac: removes logs

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -8,7 +8,6 @@ class UserController {
   public create = async (request: Request, response: Response) => {
     const user = request.body;
     const token = await this.userService.create(user);
-    // console.log('token controller --->>', user);
 
     response
       .status(StatusCodes.CREATED)
@@ -17,9 +16,7 @@ class UserController {
 
   public login = async (request: Request, response: Response) => {
     const { username } = request.body;
-    // console.log('lOG DO USER ---->>', username);
     const token = await this.userService.login(username);
-    // console.log('login controller ---->', result);
 
     response
       .status(StatusCodes.OK)
