b35c584fc14fa7c7b5831727ecce6be607fbabaf
Thiago Mota <th.araujomota@gmail.com>
2022-09-27 19:45:20 -0300

    adds validations to user routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,21 @@
 import { Router } from 'express';
 import UserController from '../controllers/user.controller';
 import { validateUsername, validatePassword, checkUser } from '../middlewares/loginValidation';
+import { validateUserName, validateClasse,
+  validateLevel, validateUserPassword } from '../middlewares/userValidation';
 
 const router = Router();
 
 const userController = new UserController();
 
-router.post('/users', userController.create);
+router.post(
+  '/users',
+  validateUserName,
+  validateClasse,
+  validateLevel,
+  validateUserPassword,
+  userController.create,
+);
 router.post('/login', validateUsername, validatePassword, checkUser, userController.login);
 
 export default router;
