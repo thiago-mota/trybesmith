b44241e83525f96fc6c549ff340e973d0e36db96
Thiago Mota <th.araujomota@gmail.com>
2022-09-27 14:36:00 -0300

    feat: adds get all users query

--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -12,13 +12,15 @@ export default class OrderModel {
     const [result] = await this.connection
       .execute(`
       SELECT 
-      orders.id, orders.userId AS productsIds
+      orders.id, orders.userId, JSON_ARRAYAGG(product.id) AS productsIds
   FROM
       Trybesmith.Orders AS orders
           INNER JOIN
       Trybesmith.Products AS product ON orders.id = product.orderId
+      GROUP BY orders.id
+      ORDER BY orders.userId
       `);
-
+    // https://dev.mysql.com/doc/refman/5.7/en/aggregate-functions.html#function_json-arrayagg
     console.log(result);
     return result as Order[];
   }
