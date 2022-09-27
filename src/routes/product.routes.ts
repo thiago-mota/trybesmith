43b2ab8d6190ebaefbef563d821b9747602fdb10
Thiago Mota <th.araujomota@gmail.com>
2022-09-27 00:38:16 -0300

    feat: adds post route fo creating products

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -5,5 +5,6 @@ const router = Router();
 
 const productController = new ProductController();
 router.get('/products', productController.getAll);
+router.post('/products', productController.create);
 
 export default router;
