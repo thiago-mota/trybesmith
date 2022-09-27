4f66238d62ab1988eb8af1744c0d84c0eb22541f
Thiago Mota <th.araujomota@gmail.com>
2022-09-27 19:08:09 -0300

    feat: adds product routes

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import ProductController from '../controllers/product.controllers';
+import { validateName, validateAmount } from '../middlewares/productValidation';
 
 const router = Router();
 
 const productController = new ProductController();
 router.get('/products', productController.getAll);
-router.post('/products', productController.create);
+router.post('/products', validateName, validateAmount, productController.create);
 
 export default router;
