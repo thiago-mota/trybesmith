31650726c96e9ee83cf240966890a47957098bd1
Thiago Mota <th.araujomota@gmail.com>
2022-09-28 09:31:49 -0300

    feat: creates routes for order

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import OrderController from '../controllers/order.controller';
+import { validateToken, validateProducts } from '../middlewares/orderValidation';
 
 const router = Router();
 
 const orderController = new OrderController();
 router.get('/orders', orderController.getAll);
+router.post('/orders', validateToken, validateProducts, orderController.create);
 
 export default router;
