40d2f28134ecb359633e2f50d7aadaf33e287719
Thiago Mota <th.araujomota@gmail.com>
2022-09-28 09:36:36 -0300

    feat: calls create function and send response

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -1,5 +1,6 @@
-import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 import OrderService from '../services/order.service';
 
 class OrderController {
@@ -11,6 +12,18 @@ class OrderController {
     response
       .status(StatusCodes.OK)
       .json(Orders);
+  };
+
+  public create = async (request: Request, response: Response) => {
+    const { productsIds } = request.body;
+    const { authorization } = request.headers;
+    
+    const tokenData = jwt.verify(authorization as string, 'secret') as jwt.JwtPayload;
+    console.log('LOG DO JWT VERIFY ---->', tokenData.result[0].id);
+
+    const newOrder = await this.orderService.create(productsIds, tokenData.result[0].id);
+
+    return response.status(StatusCodes.CREATED).json(newOrder);
   };
 }
 
